b4c88c88ccb68ac23289e1856c8d5d3b66eaf140
Mylena Silva <mylenasilvan.es@gmail.com>
2026-06-25 10:16:36 -0300

    add correção na conexão com o bd

--- src/database/connection.js
+++ src/database/connection.js
@@ -6,17 +6,7 @@ const pool = new Pool({
   ssl: {
     rejectUnauthorized: false,
   },
-}); ///Usar quando for pra produção, pois o railway exige ssl
-
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
 });
-
-require("dotenv").config();
-
-//console.log("DATABASE_URL =", process.env.DATABASE_URL);
-
-module.exports = pool;
 
 pool.query("SELECT NOW()", (err, res) => {
   if (err) {
@@ -25,3 +15,5 @@ pool.query("SELECT NOW()", (err, res) => {
     console.log("✅ Banco conectado com sucesso!");
   }
 });
+
+module.exports = pool;
