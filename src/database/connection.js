1a645fa769e98f84ac9027eb10d9a075c91446fa
Mylena Silva <mylenasilvan.es@gmail.com>
2026-06-25 10:07:01 -0300

    add parte comentada

--- src/database/connection.js
+++ src/database/connection.js
@@ -1,12 +1,12 @@
 const { Pool } = require("pg");
 require("dotenv").config();
 
-/*const pool = new Pool({
+const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
     rejectUnauthorized: false,
   },
-});*/ ///Usar quando for pra produção, pois o railway exige ssl
+}); ///Usar quando for pra produção, pois o railway exige ssl
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
